0dd013ab9cce5e053114062cf5ca09403ff6e3d9
petermarsh790@gmail.com <petermarsh790@gmail.com>
2022-01-31 15:05:44 +0200

    apostrophe apocolypse

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
             <Head>
-                <title>Pete&apos;s Website</title>
+                <title>Petes Website</title>
                 <meta name="Peter Marsh Personal Website" content="<Curriculum Vitae, Portfolio, Contact Details" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
